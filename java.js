d3eb89ddeffd8b90dda02a215bdd499dddc05f30
Daniel Ndunyo <goldendarkness365@gmail.com>
2026-01-30 12:14:10 +0300

    commit

---
+++ java.js
@@ -0,0 +1,19 @@
+<!-- Swiper JS CDN -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<script>
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+</script>
