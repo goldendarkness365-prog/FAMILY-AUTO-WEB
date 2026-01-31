32fd39fea7657ed1e8178a78a82f912965493fe3
Daniel <goldendarkness365@gmail.com>
2026-01-31 14:14:20 +0300

    commit

--- java.js
+++ java.js
@@ -1,22 +1,3 @@
-<!-- Swiper JS CDN -->
-<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
-<script>
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    centeredSlides: true,
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-</script>
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 4, // increase number of visible cars
   spaceBetween: 40, // more spacing between cars
